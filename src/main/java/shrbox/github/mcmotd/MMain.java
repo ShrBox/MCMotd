97f6a8e702075f78dbd6a0c771f0bbe29217e3cf
ShrBox <shrbox@outlook.com>
2020-07-23 20:14:54 +0800

    0.1.1 added multithread

--- src/main/java/shrbox/github/mcmotd/MMain.java
+++ src/main/java/shrbox/github/mcmotd/MMain.java
@@ -13,26 +13,8 @@ class MMain extends PluginBase {
         getEventListener().subscribeAlways(GroupMessageEvent.class, (GroupMessageEvent e) -> {
             String msg = e.getMessage().contentToString();
             if (msg.contains("!motdpe ")) {
-                String domain = msg.replace("!motdpe","").toLowerCase().trim();
-                if(domain.equals("")) {
-                    e.getGroup().sendMessage(MessageUtils.newChain(new At(e.getSender()))
-                    .plus("请输入一个地址"));
-                }
-                String port = "19132";
-                if(domain.contains(":")) {
-                    port = msg.split(":")[1];
-                }
-                String Json = Connection.getURL(domain,port);
-                if (Json.equals("0")) return;
-                Gson gson = new Gson();
-                Serverinfo serverinfo = gson.fromJson(Json,Serverinfo.class);
-                if(serverinfo.status.equals("offline")) {
-                    e.getGroup().sendMessage(MessageUtils.newChain(new At(e.getSender()))
-                    .plus("服务器不在线"));
-                    return;
-                }
-                e.getGroup().sendMessage(MessageUtils.newChain(new At(e.getSender()))
-                .plus("\n[MCMotd for Mirai]\nIP: "+domain+"\nMotd: "+serverinfo.motd+"\n协议版本: "+serverinfo.agreement+"\n游戏版本: "+serverinfo.version+"\n在线: "+serverinfo.online+"/"+serverinfo.max+"\n游戏模式: "+serverinfo.gamemode));
+                MThread mThread = new MThread();
+                mThread.boot(e);
             }
         });
     }
